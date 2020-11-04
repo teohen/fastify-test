88e4b301e0afafc19a2db383b3b34e9f14125f5d
teohen <teodorohenriquecarvalho@gmail.com>
2020-11-04 20:19:13 -0300

    chore(server): creating the server and adding a augmentation to the Fastify types for the blipp lib

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { ServerResponse, IncomingMessage, Server } from 'http';
+import fastify, {FastifyInstance} from 'fastify'
+
+import routes from "./routes";
+
+const server: FastifyInstance<
+  Server,
+  IncomingMessage,
+  ServerResponse
+> = fastify();
+
+server.register(require("fastify-blipp"));
+server.register(routes)
+
+const start = async () =>{
+    try {
+        await server.listen(3000, "localhost");
+        console.log('teste')
+        server.blipp();
+    } catch (err) {
+        console.log(err);
+        server.log.error(err);
+        process.exit(1);
+    }
+};
+
+process.on("uncaughtException", error => {
+    console.error(error);
+  });
+process.on("unhandledRejection", error => {
+  console.error(error);
+});
+  
+start();
